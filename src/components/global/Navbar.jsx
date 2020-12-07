8055ab6c816f66588a040061d9c662862e990453
ciroogg <ciroo.1993@gmail.com>
2020-12-07 14:26:41 -0300

    Navbar con alerts provisoria

--- src/components/global/Navbar.jsx
+++ src/components/global/Navbar.jsx
@@ -1,13 +1,30 @@
+import logo from './assets/logotest.png'
 function Navbar(){
+    
+
+//Provisorio
+    const alertHome = () =>{
+        alert('Va a ir a Home')
+    }
+    const alertProduts = () =>{
+        alert('Va a ir a Products')
+    }
+    const alertHotsale = () =>{
+        alert('Va a ir a HotSale')
+    }
+    const alertAboutUs = () =>{
+        alert('Va a ir a About Us')
+    }
+    
     return(
         <>  
             <nav>          
                 <ul>
-                    <li><img src="assets/logotest.png"></img></li> 
-                    <li><a href="https://www.bitdegree.org/" target="_blank">Link1</a></li>
-                    <li><a href="https://www.bitdegree.org/" target="_blank">Link2</a></li>
-                    <li><a href="https://www.bitdegree.org/" target="_blank">Link3</a></li>
-                    <li><a href="https://www.bitdegree.org/" target="_blank">Link4</a></li>
+                    <li><img src={logo} alt="Logo"/></li> 
+                    <li><a href="" onClick={alertHome}>Home</a></li>
+                    <li><a href="" onClick={alertProduts}>Productos</a></li>
+                    <li><a href="" onClick={alertHotsale}>Hotsale</a></li>
+                    <li><a href="" onClick={alertAboutUs}>About us</a></li>
                 </ul>
             </nav>
         </>
