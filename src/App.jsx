1dc42a53de858b53787710a17f3b5ff54eaf49bd
Ciro Manuel Gonzalez <ciro_@live.com.ar>
2020-12-07 13:21:38 -0300

    inicio navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './styles/App.css'
 import Hero from './components/Home/Hero'
 import Footer from './components/global/Footer'
+import Navbar from './components/global/Navbar';
 
 function App() {
   return (
     <>
+      <Navbar/>
       <Hero /> 
       <Footer /> 
     </>
---
+++ src/components/global/Navbar.jsx
@@ -0,0 +1,17 @@
+function Navbar(){
+    return(
+        <>  
+            <nav>          
+                <ul>
+                    <li><img src="assets/logotest.png"></img></li> 
+                    <li><a href="https://www.bitdegree.org/" target="_blank">Link1</a></li>
+                    <li><a href="https://www.bitdegree.org/" target="_blank">Link2</a></li>
+                    <li><a href="https://www.bitdegree.org/" target="_blank">Link3</a></li>
+                    <li><a href="https://www.bitdegree.org/" target="_blank">Link4</a></li>
+                </ul>
+            </nav>
+        </>
+    )
+}
+
+export default Navbar;
